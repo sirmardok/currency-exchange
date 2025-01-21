1c93ee5e21ca05b384357806010c26dd4d6703a1
MardoK <sirmardok@gmail.com>
2025-01-21 13:27:22 -0500

    Fixing bugs, adding error classes

--- src/main/java/com/exchange/currency_exchange/service/ExchangeApiService.java
+++ src/main/java/com/exchange/currency_exchange/service/ExchangeApiService.java
@@ -1,11 +1,16 @@
 package com.exchange.currency_exchange.service;
 
 import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.exchange.currency_exchange.adapter.RestInterface;
 import com.exchange.currency_exchange.dto.ConvertionRates;
+import com.exchange.currency_exchange.utilities.Log;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -19,14 +24,21 @@ public class ExchangeApiService {
     private String BASE_URL;
     private String ACCESS_KEY;    
 
+    static final Logger logger = LoggerFactory.getLogger(ExchangeApiService.class);
+    
 	public ConvertionRates getLatest(String base) {
+		
+		
 
         URI targetUrl = UriComponentsBuilder.fromUriString(BASE_URL)
-                                            .path("/v1/latest?access_key=" + ACCESS_KEY)
+                                            .path("/v1/latest")
                                             .queryParam("base", base)
+                                            .queryParam("access_key", ACCESS_KEY)
                                             .build()
                                             .encode()
                                             .toUri();
+        
+        logger.info("URI: " + targetUrl );
 
         return restInterface.getCurrencyInfo(targetUrl, ConvertionRates.class);
     }
